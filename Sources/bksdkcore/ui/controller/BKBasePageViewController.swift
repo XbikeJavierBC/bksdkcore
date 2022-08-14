9c60942e6262ce7f08147663a95207ff7a123a5d
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2022-08-14 00:41:17 -0500

    Se agregan controladores base

---
+++ Sources/bksdkcore/ui/controller/BKBasePageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Javier Carapia on 14/08/22.
+//
+
+import UIKit
+import ghmjolnircore
+
+open class BKBasePageViewController: UIPageViewController, GHBaseViewControllerDelegate {
+    public var controllerType: Int?
+    public var bundle: GHBundleParameters?
+    public var viewModel: GHBaseViewModelProtocol?
+    public var controllerManager: GHManagerController?
+    
+    open override var preferredStatusBarStyle: UIStatusBarStyle { .default }
+    open override var prefersStatusBarHidden: Bool { false }
+    
+    open override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        GHCacheImage.shared.releaseCache()
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        debugPrint("✅✅✅ \(self.description) ✅✅✅")
+    }
+    
+    open func removeReferenceContext() {
+        self.bundle = nil
+        self.controllerType = nil
+        self.controllerManager = nil
+        self.viewModel = nil
+    }
+}
